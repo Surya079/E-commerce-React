32dd75dc0f19d9008f3e689e18081d70f4c358c3
Surya <surya.vme005@gmail.com>
2024-08-10 23:26:52 +0530

    component updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,84 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Logo from "../assets/logo.svg";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/Icons/logo.svg";
+import logout from "../assets/Icons/logout.svg";
+import login from "../assets/Icons/User.svg";
 import { Navbar } from "./Navbar";
 import { MdClose, MdMenu } from "react-icons/md";
+import { FaOpencart } from "react-icons/fa";
+import { Login } from "../pages/Login";
 
 export const Header = () => {
   // Menu button
-  const [menuOpened, setmenuOpened] = useState(false);
+  const bolValue = false;
 
-  const toggleMenu = () => setmenuOpened(!menuOpened)
+  const [menuOpened, setmenuOpened] = useState(bolValue);
+
+  const toggleMenu = () => setmenuOpened(!menuOpened);
 
   return (
     <div>
       <header className="fixed top-0 left-0 m-auto w-full bg-white px-6  ring-gray-100 ring-1 z-10">
         <div className="px-4 p-3 flex justify-between">
           {/* Logo */}
-          <div>
+          <div className="flex items-center">
             <Link>
-              <img src={Logo} alt="" height={60} width={60} />
+              <img src={Logo} alt="" height={40} width={40} />
             </Link>
           </div>
 
           {/* Navbar  Desktop */}
           <Navbar
-            continerStyles={"hidden md:flex gap-x-5 xl:gap-x-10 medium-15"}
+          
+            continerStyles={"hidden md:flex gap-x-5 xl:gap-x-10 text-yellow-600 "}
+            
           />
 
           {/* Navbar Mobile */}
           <Navbar
             continerStyles={`${
-              menuOpened
-                ? "flex item-start flex-col gap-y-12 fixed top-20 right-8 p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-gray-100 ring-1 transition-all duration-300"
-                : "flex item-start flex-col gap-y-12 fixed top-20  p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-state-900/5 transition-all duration-300 -right-[100%]"
+              menuOpened 
+                ? "flex md:hidden item-start flex-col text-white gap-y-12 fixed top-20 right-8 p-12 bg-yellow-300 rounded-3xl shadow-md w-64 medium-16 ring-gray-100 ring-1 transition-all duration-300"
+                : "flex  item-start flex-col text-white gap-y-12 fixed top-20  p-12 bg-white rounded-3xl shadow-md w-64 medium-16 ring-1 ring-state-900/5 transition-all duration-300 -right-[100%]"
             }`}
           />
 
           {/* Buttons */}
-          <div>
+          <div className="flex justify-center  bold-16">
             {!menuOpened ? (
-              <MdMenu className="md:hidden cursor-pointer hover:text-red-800 mr-2 p-1 ring-1 ring-slate-900 h-8 w-8 rounded-full" onClick={toggleMenu} />
+              <MdMenu
+                className="md:hidden cursor-pointer hover:text-yellow-300 mr-2 p-1 ring-1 ring-slate-900 h-8 w-8 rounded-full"
+                onClick={toggleMenu}
+              />
             ) : (
-              <MdClose className="md:hidden cursor-pointer hover:text-red-800 mr-2 p-1 ring-1 ring-slate-900 h-8 w-8 rounded-full" onClick={toggleMenu} />
+              <MdClose
+                className="md:hidden cursor-pointer hover:text-yellow-300 mr-2 p-1 ring-1 ring-slate-900 h-8 w-8 rounded-full"
+                onClick={toggleMenu}
+              />
             )}
+
+            {/* Cart Page */}
+
+            <div className="flex justify-center  ">
+              <NavLink to={"cart-page"} className={"flex"}>
+                <FaOpencart className="p-1 h-8 w-8 ring-1 ring-slate-900 rounded-full" />
+                <span className="relative flex items-center justify-center h-5 w-5 rounded-full bg-yellow-300 -top-1">
+                  0
+                </span>
+              </NavLink>
+              {/* <NavLink to={"logout"} className={"bg-yellow-300 ml-2 py-1 h-8 w-20 rounded-2xl gap-x-1 justify-center items-center flex text-sm"}>
+               <img src={logout} alt="logutIcon" width={19} height={19} /> Logut
+              </NavLink> */}
+              <NavLink
+                to={"login"}
+                className={
+                  "bg-yellow-300 ml-2 py-1 h-8 w-20 rounded-2xl gap-x-1 justify-center flex items-center"
+                }
+              >
+                <img src={login} alt="UserProfile" width={19} height={19} />
+                login
+              </NavLink>
+            </div>
           </div>
         </div>
       </header>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdCategory, MdContacts, MdHomeFilled, MdShop2 } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
 export const Navbar = ({ continerStyles }) => {
   return (
     <div className={`${continerStyles}`}>
-      <NavLink to={"/"}>
-        <div className="flex items-center gap-x-1">
+      <NavLink
+        to={"/"}
+        className={({ isActive }) => (isActive ? "active_link" : "")}
+      >
+        <div className="flex items-start gap-x-1">
           <MdHomeFilled />
           Home
         </div>
       </NavLink>
-      <NavLink to={"/mens"}>
-        <div className="flex items-center gap-x-1">
+      <NavLink
+        to={"/mens"}
+        className={({ isActive }) => (isActive ? "active_link" : "")}
+      >
+        <div className="flex items-start gap-x-1">
           <MdCategory />
           Men's
         </div>
       </NavLink>
-      <NavLink to={"/womens"}>
-        <div className="flex items-center gap-x-1">
+      <NavLink
+        to={"/womens"}
+        className={({ isActive }) => (isActive ? "active_link" : "")}
+      >
+        <div className="flex items-start gap-x-1">
           <MdShop2 />
           Women's
         </div>
       </NavLink>
-      <NavLink to={"/kids"}>
-        <div className="flex items-center gap-x-1">
+      <NavLink
+        to={"/kids"}
+        className={({ isActive }) => (isActive ? "active_link" : "")}
+      >
+        <div className="flex items-start gap-x-1">
           <MdContacts />
           Kid's
         </div>
       </NavLink>
-     
     </div>
   );
 };
