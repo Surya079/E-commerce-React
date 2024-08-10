0dade826c6375c12127a7dbb423adae92c0dee3f
Surya <surya.vme005@gmail.com>
2024-08-10 18:10:35 +0530

    Routes created for allpages and component

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+import { Category } from "./pages/Category";
+import { Cart } from "./pages/Cart";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Products } from "./pages/products";
+import { Footer } from "./components/Footer";
+
+export default function App() {
+  return (
+    <main className="bg-primary text-tertiary">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mens" element={<Category />} />
+          <Route path="/womens" element={<Category />} />
+          <Route path="/kids" element={<Category />} />
+          <Route path="/products" element={<Products />}>
+            <Route path=":productId" element={<Products />} />
+          </Route>
+          <Route path="/cart-page" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </main>
+  );
+}
